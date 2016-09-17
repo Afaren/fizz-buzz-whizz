21a27472d5ddda6b2f37251c049974c1c3c74bf6
Afaren <550047450@qq.com>
2016-09-17 18:42:25 +0800

    extract method

--- src/NumberReporter.java
+++ src/NumberReporter.java
@@ -1,24 +1,26 @@
 /**
  * Created by afaren on 9/17/16.
  */
+
 import java.util.Map;
 import java.util.HashMap;
+
 public class NumberReporter {
 
 
     private int first;
     private int second;
     private int third;
-    private Map<Integer,String> pairs=new HashMap();
+    private Map<Integer, String> pairs = new HashMap();
 
     public NumberReporter(int first, int second, int third) {
         this.first = first;
         this.second = second;
         this.third = third;
 
-	pairs.put(first,"Fizz");
-	pairs.put(second,"Buzz");
-	pairs.put(third,"Whizz");
+        pairs.put(first, "Fizz");
+        pairs.put(second, "Buzz");
+        pairs.put(third, "Whizz");
     }
 
     public Object report(int number) {
@@ -27,26 +29,30 @@ public class NumberReporter {
             return pairs.get(first);
         }
 
-	return canBeMod(number) ? modNumberToTag(number)  :new Integer(number);
+        return canBeMod(number) ? modNumberToTag(number) : new Integer(number);
     }
 
-    private boolean canBeMod(int number){
-	int n  = number;
-	return n % first == 0 || n % second == 0 || n % third == 0;
+    private boolean canBeMod(int n) {
+        return canBeModBy(first, n) || canBeModBy(second, n) || canBeModBy(third, n);
     }
-    
+
+    private boolean canBeModBy(int moder, int number) {
+        return number % moder == 0;
+    }
+
     private String modNumberToTag(int number) {
         String result = "";
-        if (number % first == 0)
+        if (canBeModBy(first, number))
             result += pairs.get(first);
 
-        if (number % second == 0)
+        if (canBeModBy(second, number))
             result += pairs.get(second);
 
-        if (number % third == 0)
+        if (canBeModBy(third, number))
             result += pairs.get(third);
         return result;
     }
+
 
     private boolean containsFirst(int number) {
         return (number + "").contains(this.first + "");
