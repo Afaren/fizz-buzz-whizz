d45fe95e612385a20a1740273024ddd85d33d645
Afaren <550047450@qq.com>
2016-09-17 15:49:15 +0800

    modify struction

--- test/NumberReporterTest.java
+++ test/NumberReporterTest.java
@@ -31,21 +31,22 @@ public class NumberReporterTest {
 	
     }
     @Test
-    public void should_report_special_string_when_given_double_number() throws Exception {
+    public void should_report_combine_string_when_can_be_mod_by_more_than_one_rule() throws Exception {
 	assertReport(15, "FizzBuzz");
 	assertReport(21, "FizzWhizz");
 	assertReport(70, "BuzzWhizz");
+	assertReport(105, "FizzBuzzWhizz");
     }
 
     @Test
-    public void should_report_special_string_when_given_contain_three() throws Exception {
+    public void should_report_Fizz_when_contains_first_number()throws Exception {
 	assertReport(13, "Fizz");
     }
 
     @Test
-    public void should_report_special_string_when_given_contain_three_and_double_numbers() throws Exception {
+    public void should_report_Fizz_when_contains_first_number_and_can_be_mod() throws Exception {
 	assertReport(35, "Fizz"); // 5 * 7
-	assertReport(105, "FizzBuzzWhizz");
+
     }
 
 
